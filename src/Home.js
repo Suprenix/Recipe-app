52b9a99ca8b43c9cf7d9df98aefc23e235bdcc06
Suprenix <suprenix@gmail.com>
2025-02-13 10:25:01 +0100

    Linked home page to database and implemented 'recipe details' on home screen

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react";
+import RecipeList from "./RecipeList";
+
+
 const Home = () => {
+    const [recipes, setRecipes] = useState(null);
+    
+
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/recipes')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            setRecipes(data);
+        })
+    }, []);
     return ( 
-        <h1>Hello, World!</h1>
+        <div>{recipes && <RecipeList recipes = {recipes} setRecipes = {recipes} />}</div>  
      );
 }
  
